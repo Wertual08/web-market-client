be1419e18baeac89598b4250c395f58506d881da
Wertual <wertual08@gmain.com>
2021-07-10 19:43:07 +0300

    Local configuration added.

--- project/vue.config.js
+++ project/vue.config.js
@@ -1,10 +1,10 @@
-//module.exports = {
-//  devServer: {
-//    proxy: {
-//      '/api': {
-//        target: 'http://84.252.141.174',
-//        changeOrigin: true,
-//      }
-//    }
-//  }
-//}
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://84.252.141.174',
+        changeOrigin: true,
+      }
+    }
+  }
+}
