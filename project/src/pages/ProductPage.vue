899b50af8df4adf3c6e47e99abe6b1949aad64da
Ivan Smolenchuk <ivansmolencuk@yandex.ru>
2021-08-18 19:31:04 +0300

    prod card start

--- project/src/pages/ProductPage.vue
+++ project/src/pages/ProductPage.vue
@@ -1,8 +1,13 @@
-<template>
+<template class = 'card'>
+<div class="abc">
+  <Menu />
+  <img class="magamed" :src="getCoverImage(product)" alt="../assets/meme.gif" />
   <div>{{ product.id }}</div>
-  <div>{{ product.name }}</div>
+  <nobr>{{ product.name }}</nobr>
   <div>{{ product.description }}</div>
-  <img :src="getCoverImage(product)" alt="../assets/meme.gif" />
+  <p>1</p>
+</div>
+
   <div></div>
 </template>
 
@@ -10,9 +15,13 @@
 import { defineComponent } from "vue";
 import Repa from "@/repositories/repository";
 import Product from "@/models/product";
+import Menu from "@/components/DropdownMenu.vue";
 
 export default defineComponent({
   name: "ff",
+  components: {
+    Menu,
+  },
   data() {
     return { product: new Product(), id: null as any };
   },
@@ -37,4 +46,16 @@ export default defineComponent({
 
 
 <style scoped>
+.abc{
+background: rgb(9,9,121);
+background: linear-gradient(90deg, rgba(9,9,121,1) 0%, rgba(0,129,255,0.16290266106442575) 79%, rgba(2,0,36,1) 100%);
+}
+.magamed {
+  float: right;
+  top: 0pc;
+  width: auto;
+  width: 500px;
+  height: auto;
+}
+
 </style>
