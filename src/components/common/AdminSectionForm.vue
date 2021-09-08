99ca617269a620d5d0b519a3e56a827658c4cc38
Wertual <wertual08@gmain.com>
2021-09-08 12:19:46 +0300

    Working on sections redactor.

---
+++ src/components/common/AdminSectionForm.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="box">
+    {{ localSection.id }}
+  </div>
+</template>
+
+
+<style scoped>
+
+</style>
+
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import SectionsRepository from '@/repositories/admin/sectionsRepository'
+import Section from '@/models/admin/section'
+
+export default defineComponent({
+  name: 'admin-section-form',
+
+  props: {
+    section: {
+      type: Object as PropType<Section>,
+    }
+  },
+
+  setup() {
+    return {
+      sectionsRepository: new SectionsRepository(),
+    }
+  },
+
+  data() {
+    return {
+      localSection: new Section(),
+    }
+  },  
+
+  watch: {
+    section(value: Section) {
+      this.localSection = value
+    }
+  },
+})
+</script>
